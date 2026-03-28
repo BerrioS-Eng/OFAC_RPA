e723a63de01aabd5aa8706771772d5da8f5412c4
sberrio <sberrio09@gmail.com>
2026-03-28 13:30:08 -0500

    feat: add person classification services Add ClassificationResult dataclasses and classify_persons function that splits persons into three groups: web process candidates, incompletes records, and those with no match in MaestraDetallePersonas

---
+++ services/classify_persons.py
@@ -0,0 +1,36 @@
+import logging
+from dataclasses import dataclass, field
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class ClassificationResult:
+    """"""
+    candidates: list = field(default_factory= list)
+    incompletes: list = field(default_factory= list)
+    not_cross: list = field(default_factory= list)
+
+
+def classify_persons(registers):
+    """"""
+    result = ClassificationResult()
+
+    for reg in registers:
+        if not reg["cruzaConMaestra"]:
+            result.not_cross.append(reg)
+        
+        elif reg["direccion"] is None or reg["pais"] is None:
+            result.incompletes.append(reg)
+
+        else:
+            result.candidates.append(reg)
+    
+    logger.info(
+        "Classification made: ",
+        f"{len(result.candidates)} web process candidates",
+        f"{len(result.incompletes)} incomplete information",
+        f"{len(result.not_cross)} they do not cross with MaestraDetallePersonas"
+    )
+
+    return result
+
